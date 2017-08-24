a5edeef408ae30c364051eb0f37ae30e3b8fc984
Ed <eddyburt4@gmail.com>
2017-08-24 20:36:26 +0100

    cleaning up theme bs

--- src/main/themes.js
+++ src/main/themes.js
@@ -1,22 +1,10 @@
-import {
-  darkBlack,
-  fullBlack,
-  grey400,
-  grey500,
-  grey700,
-  blue900
-} from "material-ui/styles/colors";
+import { grey400, grey700, blue900 } from "material-ui/styles/colors";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
 
 export const muiTheme = getMuiTheme({
   palette: {
     primary1Color: blue900,
     textColor: grey400,
-    alternateTextColor: grey500,
-    borderColor: darkBlack,
-    pickerHeaderColor: blue900,
-    shadowColor: fullBlack,
-    borderColor: grey500,
-    disabledTextColor: grey500
+    borderColor: grey700
   }
 });
