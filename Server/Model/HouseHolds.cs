8413b1c36d4918e01ffb1be8c0c22e6300176b27
Ed <eddyburt4@gmail.com>
2017-10-31 18:40:25 +0000

    moving server out to separate repo

--- Server/Model/HouseHolds.cs
+++
@@ -1,7 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-public class HouseHold {
-    [Key]
-    public string HouseHoldId { get; set; }
-    public string Name { get; set; }
-}
--- Server/Model/Users.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-public class User {
-    [Key]
-    public string UserId { get; set; }
-    public string DisplayName { get; set; }
-    public int HouseHoldId { get; set; }
-}
