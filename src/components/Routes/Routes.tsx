4fdba574eb47b68ee23a2967f91f6da550c13011
Ed <eddyburt4@gmail.com>
2018-03-07 19:43:25 +0000

    wondering if we can have stateless

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -10,49 +10,38 @@ import HouseSummary from '../HouseSummary';
 import NotFound404 from '../NotFound404';
 import { checkAuthorization } from '../Occupants/occupantsActions';
 
-class Routes extends React.Component<RouteComponentProps<any> & IRoutesProps> {
-    getRoutes() {
-        checkAuthorization(this.props.loggedInOccupant);
-        let routes: JSX.Element;
-        if (this.props.isLoggedIn) {
-            routes = (
-                <Switch>
-                    <Route exact path="/" component={Balance} />
-                    <Route path="/Balance" component={Balance} />
-                    <Route path="/ViewTransactions" component={ViewTransactions} />
-                    <Route path="/AddTransaction" component={AddTransaction} />
-                    <Route path="/HouseSummary" component={HouseSummary} />
-                    <Route exact path="*" component={NotFound404} />
-                </Switch>
-            );
-        } else {
-            routes = (
-                <Switch>
-                    <Route exact path="*" component={NotFound404} />
-                </Switch>
-            ); // TODO: Have an recieve handle sign in / recieve occupant action which accepts url params from myhouse
-        }
-        return routes;
-    }
+// class Routes extends React.Component<RouteComponentProps<any> & IRoutesProps> {
+//     getRoutes() {
+//         checkAuthorization(this.props.loggedInOccupant);
+//         let routes: JSX.Element;
+//         if (this.props.isLoggedIn) {
+//             routes = (
+//                 <Switch>
+//                     <Route exact path="/" component={Balance} />
+//                     <Route path="/Balance" component={Balance} />
+//                     <Route path="/ViewTransactions" component={ViewTransactions} />
+//                     <Route path="/AddTransaction" component={AddTransaction} />
+//                     <Route path="/HouseSummary" component={HouseSummary} />
+//                     <Route exact path="*" component={NotFound404} />
+//                 </Switch>
+//             );
+//         } else {
+//             routes = (
+//                 <Switch>
+//                     <Route exact path="*" component={NotFound404} />
+//                 </Switch>
+//             ); // TODO: Have an recieve handle sign in / recieve occupant action which accepts url params from myhouse
+//         }
+//         return routes;
+//     }
 
 
-    render() {
-        return <Route>{gubs}</Route>;
-    }
-}
+//     render() {
+//         return <Route>{gubs}</Route>;
+//     }
+// }
 
-const mapStateToProps = (store: any) => {
-    return {
-        isLoggedIn: store.navReducer.isLoggedIn,
-        loggedInOccupant: store.navReducer.loggedInOccupant,
-    };
-};
-
-const ConnectedRoutes = withRouter(connect(mapStateToProps)(Routes));
-export default ConnectedRoutes;
-
-
-const gubs: React.StatelessComponent<IRoutesProps> = (props) => {
+const Routes: React.StatelessComponent<RouteComponentProps<any> & IRoutesProps> = (props) => {
     checkAuthorization(props.loggedInOccupant);
     let routes: JSX.Element;
     if (props.isLoggedIn) {
@@ -75,4 +64,17 @@ const gubs: React.StatelessComponent<IRoutesProps> = (props) => {
     }
     return routes;
 };
-export { gubs };
+export { Routes };
+
+const mapStateToProps = (store: any) => {
+    return {
+        isLoggedIn: store.navReducer.isLoggedIn,
+        loggedInOccupant: store.navReducer.loggedInOccupant,
+    };
+};
+
+const ConnectedRoutes = withRouter(connect(mapStateToProps)(Routes));
+export default ConnectedRoutes;
+
+
+
