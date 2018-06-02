ae5612bdf0597fa75af2d6bd1a42f7aeba565dee
Ed <eddyburt4@gmail.com>
2018-06-02 10:35:51 +0100

    rehydrate needs to be more intelligent

--- src/main/store.ts
+++ src/main/store.ts
@@ -2,7 +2,7 @@ import * as localForage from 'localforage';
 import { routerMiddleware } from 'react-router-redux';
 import { Middleware, Store, applyMiddleware, compose, createStore } from 'redux';
 import logger from 'redux-logger';
-import { PersistorConfig, autoRehydrate, persistStore } from 'redux-persist';
+import { PersistorConfig, persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
 import history from './history';
 import combinedReducers from './reducers';
@@ -12,7 +12,8 @@ const rmiddleware: Middleware = routerMiddleware(history);
 const store: Store<{}> = createStore(
   combinedReducers,
   undefined,
-  compose(applyMiddleware(thunk, logger, rmiddleware), autoRehydrate()),
+  compose(applyMiddleware(thunk, logger, rmiddleware)),
+  // autoRehydrate()), Currently this screws the pass through auth 
 );
 
 const persistConfig: PersistorConfig = {
@@ -20,7 +21,7 @@ const persistConfig: PersistorConfig = {
     storage: localForage,
 };
 
-persistStore(store).purge();
+// For Dev persistStore(store).purge();
 persistStore(store, persistConfig);
 
 export default store;
