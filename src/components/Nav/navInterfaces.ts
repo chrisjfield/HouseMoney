d7e9b56c5d153298e0b4ce20ebf31767a068afcc
Ed.Burt <ed.burt@theaccessgroup.com>
2018-06-10 10:55:35 +0100

    WIP

--- src/components/Nav/navInterfaces.ts
+++ src/components/Nav/navInterfaces.ts
@@ -1,8 +1,9 @@
-import { Action, Dispatch } from 'redux';
+import { Action, Dispatch, ActionWithPayload } from 'redux';
 import { IOccupant } from '../Occupants/occupantsInterfaces';
 
 export interface INavProps {
     dispatch: Dispatch<Action>;
+    receiveOccupant: ActionWithPayload;
     loggedInOccupant: IOccupant;
     isLoggedIn: boolean;
 }
--- src/components/Occupants/occupantsActions.ts
+++ src/components/Occupants/occupantsActions.ts
@@ -3,14 +3,13 @@ import { persistStore } from 'redux-persist';
 import { myHouseUrl } from '../../appConfig';
 import { endpoints } from '../../enums/endpointsEnum';
 import { HTTPMethod } from '../../enums/httpEnum';
-import { ActionsUnion, createAction } from '../../helpers/actionCreator';
 import apiHelper from '../../helpers/apiHelper';
 import { AuthorizationResponse } from '../../interfaces/apiInterfaces';
 import { store } from '../../main/configureStore';
 import { ErrorMessageActions } from '../ErrorMessage/errorMessageActions';
 import { LoadingActions } from '../Loading/loadingActions';
 import { ILogoutDetails, IOccupant, LogoutReason } from './occupantsInterfaces';
-import actionCreatorFactory from 'typescript-fsa';
+import { action } from 'typesafe-actions';
 
 export enum occupantActionsTypes {
     LOGGED_OUT = 'LOGGED_OUT',
@@ -64,10 +63,8 @@ export function getLogoutUrlWithDetails(logoutReason: LogoutReason) {
     return myHouseUrl + 'Logout?' + queryString.stringify(logoutDetails);
 }
 
-const occupantsActionsCreator = actionCreatorFactory();
+export const receiveOccupant = (occupant: IOccupant, isLoggedIn: boolean) =>
+    action(occupantActionsTypes.RECEIVE_OCCUPANT, { occupant, isLoggedIn });
 
-export const receiveOccupant =
-    occupantsActionsCreator<{occupant: IOccupant, isLoggedIn: boolean}>(occupantActionsTypes.RECEIVE_OCCUPANT);
-
-export const receiveHouseholdOccupants =
-    occupantsActionsCreator<{householdOccupantsArray: IOccupant[]}>(occupantActionsTypes.RECEIVE_OCCUPANTS_OF_HOUSEHOLD);
+export const receiveHouseholdOccupants = (householdOccupantsArray: IOccupant[]) =>
+    action(occupantActionsTypes.RECEIVE_OCCUPANTS_OF_HOUSEHOLD, householdOccupantsArray);
