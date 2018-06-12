79bc40f22932beb6ed95e41b370f3895ad10147e
Ed <eddyburt4@gmail.com>
2018-06-12 18:27:45 +0100

    Removing ActionsUnion (feels odd)

--- src/components/Loading/loadingActions.ts
+++ src/components/Loading/loadingActions.ts
@@ -1,16 +1,9 @@
-import { ActionsUnion, createAction } from '../../helpers/actionCreator';
-
-const loadingStarted = () => createAction(loadingActionTypes.LOADING_STARTED);
-const loadingComplete = () => createAction(loadingActionTypes.LOADING_COMPLETED);
-
-export const LoadingActions = {
-    loadingStarted,
-    loadingComplete,
-};
+import { createAction } from '../../helpers/actionCreator';
 
 export enum loadingActionTypes {
     LOADING_STARTED = 'LOADING_STARTED',
     LOADING_COMPLETED = 'LOADING_COMPLETED',
 }
 
-export type LoadingActions = ActionsUnion<typeof LoadingActions>;
+export const loadingStarted = () => createAction(loadingActionTypes.LOADING_STARTED);
+export const loadingComplete = () => createAction(loadingActionTypes.LOADING_COMPLETED);
